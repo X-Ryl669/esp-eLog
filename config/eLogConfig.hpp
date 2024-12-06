13ea99880c8f99a9331bef259d06fc2317352be3
X-Ryl669 <boite.pour.spam@gmail.com>
2024-12-06 18:16:54 +0100

    Fix building

--- config/eLogConfig.hpp
+++ config/eLogConfig.hpp
@@ -3,10 +3,22 @@
 
 // Configuration is done via macros
 #include "sdkconfig.h"
+#include "esp_attr.h"
 
 #define LogRingBufferSize       CONFIG_ESP_ELOG_BUFFER_SIZE
 #define DeleteOldLogsWhenFull   CONFIG_ESP_ELOG_DELETE_OLD_LOG_IF_FULL
-#define StoreLogSizeType        CONFIG_ESP_ELOG_SIZE_TYPE
+
+#define DontDefineStoreLogSizeType
+
+#if CONFIG_ESP_ELOG_SIZE_TYPE == 1
+   #define StoreLogSizeType uint8
+#elif CONFIG_ESP_ELOG_SIZE_TYPE == 2
+   #define StoreLogSizeType uint16
+#elif CONFIG_ESP_ELOG_SIZE_TYPE == 0
+#else
+   #warning "Invalid size for this type"
+#endif
+
 #define ThrowOnError            CONFIG_ESP_ELOG_ERROR_TYPE
 
 #ifdef CONFIG_ESP_ELOG_PERSISTENT_LOG
